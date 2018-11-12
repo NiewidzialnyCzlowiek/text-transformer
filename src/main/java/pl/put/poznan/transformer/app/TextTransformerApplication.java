da03f7d2b63875a5ba78f8c2b7f662e9d39dfc49
maciejmoscicki <moscicki.maciej619@gmail.com>
2018-11-12 18:59:37 +0100

    1. Bump spring to ^2.0.0 2. Add mock test 3. Modify project structure

--- src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
+++ src/main/java/pl/put/poznan/transformer/app/TextTransformerApplication.java
@@ -3,8 +3,7 @@ package pl.put.poznan.transformer.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@SpringBootApplication(scanBasePackages = {"pl.put.poznan.transformer.rest"})
+@SpringBootApplication
 public class TextTransformerApplication {
 
     public static void main(String[] args) {
