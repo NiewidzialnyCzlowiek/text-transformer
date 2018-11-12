dafed419c3c4edec827c2bffe844be2492ca67bb
Maciej Moscicki <moscicki.maciej619@gmail.com>
2018-11-12 19:07:32 +0100

    Modify project structure (#2)
    * 1. Bump spring to ^2.0.0
    2. Add mock test
    3. Modify project structure

---
+++ src/test/java/pl/put/poznan/transformer/ExampleTest.java
@@ -0,0 +1,11 @@
+package pl.put.poznan.transformer;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+  @Test
+  public void exampleTest() {
+    assertEquals(1, 1);
+  }
+}
