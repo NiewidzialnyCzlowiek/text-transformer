aa0135e6b05ff52b1579dfe9a84c40c3e89c64af
Bartlomiej Szal <bartlomiej.szal@gmail.com>
2018-11-13 12:11:15 +0100

    Fixed publish-docs script

--- .utils/publish-docs.sh
+++ .utils/publish-docs.sh
@@ -1,18 +1,14 @@
 #!bin/bash
-
-git commit
-git push
-
 cd $HOME
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
-git clone --quiet https://${GH_TOKEN}@github.com/NiewidzialnyCzlowiek/text-transformer > /dev/null
+git clone --quiet https://${GH_TOKEN}@github.com/NiewidzialnyCzlowiek/text-transformer text-transformer
 
 # Commit and Push the Changes
 cd text-transformer
 mvn site
 cp -Rf ./target/site ./docs
 rm -r ./target
-git add ./docs/*
+git add ./docs/**
 git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to /docs directory"
-git push -fq origin master > /dev/null
+git push -fq origin master
