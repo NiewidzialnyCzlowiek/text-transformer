9fa57d1b8553105c7abc993a7e952d7487d696c5
newt0npk <wandaxdesign@gmail.com>
2018-11-13 19:19:05 +0100

    Real time server querying + pokemonize

--- web-client/js/script.js
+++ web-client/js/script.js
@@ -1,10 +1,74 @@
+var mainForm = $("#mainForm");
+
 function alert(alerttype, message) {
   $('#alert-placeholder').html('<div id="alertdiv" class="alert ' +  alerttype + '"><span>'+message+'</span></div>')
 }
 
+function submitForm(form) {
+  var form = $(form);
+  var url = form.attr('action');
+
+  var req = {};
+
+  var trans = $('#alert-dimmer');
+
+  trans.addClass('active');
+
+  req.text = $( "#text" ).val();
+  if ($("#repetition_del").prop('checked')) {
+    req.repetition_del = true;
+  }
+  if ($("#num_to_text").prop('checked')) {
+    req.num_to_text = true;
+  }
+  if ($("#expand").prop('checked')) {
+    req.expand = true;
+  }
+  if ($("#shrink").prop('checked')) {
+    req.shrink = true;
+  }
+  if ($("#latex").prop('checked')) {
+    req.latex = true;
+  }
+
+  req.transformations = [];
+
+  $( "ul#transformations li" ).each(function( index ) {
+    req.transformations.push($(form).text());
+  });
+
+  console.log(req);
+  $("#mainFormSubmit").prop('disabled', true);
+  alert('alert-info', 'Awaiting server response...');
+
+  $("#mainFormSubmit").prop('disabled', false);
+  $.ajax({
+          type: "POST",
+        url: "http://localhost:8080/transform",
+        contentType: "application/json",
+        dataType: "json",
+          data: JSON.stringify(req),
+          success: function(data)
+          {
+            trans.removeClass('active');
+            var alertText = '<p>A result was returned: </p><figure class="highlight"><pre><code>' + data.text + '</code></pre></figure>';
+            alert('alert-success', alertText)
+            $("#mainFormSubmit").prop('disabled', false);
+
+          }
+        }).fail(function($xhr) {
+          trans.removeClass('active');
+          var data = $xhr.responseJSON;
+          var text = 'An error occured: ' + data.error;
+          alert('alert-danger', text);
+          $("#mainFormSubmit").prop('disabled', false);
+          
+        });
+}
+
 
 $( document ).ready(function() {
-    $("ul#transformations").sortable();
+    $("ul#transformations").sortable({onDrop: function() {submitForm(mainForm)}});
 
     $( "li.new-tf a" ).each(function(index) {
         $(this).on("click", function(){
@@ -15,66 +79,27 @@ $( document ).ready(function() {
 
     $( "#text" ).keyup(function () {
       $( "#char-count" ).html(this.value.length);
+      submitForm(mainForm);
     });
 
-    $("#mainForm").submit(function(e) {
-
-      var form = $(this);
-      var url = form.attr('action');
-
-      var req = {};
-
-      req.text = $( "#text" ).val();
-      if ($("#repetition_del").prop('checked')) {
-        req.repetition_del = true;
-      }
-      if ($("#num_to_text").prop('checked')) {
-        req.num_to_text = true;
-      }
-      if ($("#expand").prop('checked')) {
-        req.expand = true;
-      }
-      if ($("#shrink").prop('checked')) {
-        req.shrink = true;
-      }
-      if ($("#latex").prop('checked')) {
-        req.latex = true;
-      }
-
-      req.transformations = [];
-
-      $( "ul#transformations li" ).each(function( index ) {
-        req.transformations.push($(this).text());
-      });
-
-      console.log(req);
-      $("#mainFormSubmit").prop('disabled', true);
-      alert('alert-info', 'Awaiting server response...');
-
-      $("#mainFormSubmit").prop('disabled', false);
-      $.ajax({
-             type: "POST",
-            url: "http://localhost:8080/transform",
-            contentType: "application/json",
-            dataType: "json",
-             data: JSON.stringify(req),
-             success: function(data)
-             {
-               var alertText = '<p>A result was returned: </p><figure class="highlight"><pre><code>' + data.text + '</code></pre></figure>';
-               alert('alert-success', alertText)
-               $("#mainFormSubmit").prop('disabled', false);
-             }
-           }).fail(function($xhr) {
-             var data = $xhr.responseJSON;
-             var text = 'An error occured: ' + data.error;
-             alert('alert-danger', text);
-             $("#mainFormSubmit").prop('disabled', false);
-           });
-
+    mainForm.submit(function(e) {
+      submitForm(mainForm);
       e.preventDefault();
   });
+
+  mainForm.change(function() {
+    submitForm(this);
+  })
 });
 
 function dataTransformationRemove(el) {
   $(el).parent().remove();
+}
+
+
+function getStarted() {
+  $('#intro').fadeOut();
+  setTimeout(function() {
+    $('#mainForm').addClass('animate-fadeIn');
+  }, 200);
 }
